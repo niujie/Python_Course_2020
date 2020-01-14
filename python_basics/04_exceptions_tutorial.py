7ec8ee2fc5c95e9558c6f7ed84542ef7def44cfc
niujie <niujay@gmail.com>
2020-01-15 00:35:49 +0800

    updated

--- python_basics/04_exceptions_tutorial.py
+++ python_basics/04_exceptions_tutorial.py
@@ -1,2 +1,16 @@
 for i in range(5)
     print("Hello, world")
+
+1/0
+
+with open("file_not_exist.txt") as xf:
+    the_truth = xf.read()
+
+print(the_truth)
+
+1 + 2 + "three"
+
+import math
+print(math.sqrt(-1))
+
+
